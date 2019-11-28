089a54e07fff9f193cbdde49be1a6e423bac9eb4
Nataly <html-tida@mail.ru>
2019-11-29 02:14:37 +0300

    верстка второй секции. Слайдер

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,35 @@ $( document ).ready(function() {
   $('.ham').on('click', function(){
     $('.navbar__menu').toggleClass('active');
   });
+  var swiper = new Swiper('.projects__mainslider', {
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    pagination: {
+      el: '.mainslider-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.mainslider-button-next',
+      prevEl: '.mainslider-button-prev',
+    },
+  });
+  var swiper2 = new Swiper('.projects__photoslider', {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    nested: 'true',
+    navigation: {
+      nextEl: '.photoslider-button-next',
+      prevEl: '.photoslider-button-prev',
+    },
+    // autoplay: {
+    //   delay: 6000,
+    // },
+    // Disable preloading of all images
+    
+    // autoHeight: true,
+  });
 });
+
